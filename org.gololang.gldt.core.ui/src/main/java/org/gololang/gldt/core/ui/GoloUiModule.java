094822fb27d12aa57f3d8631da3c298b7588c8d3
Jeff MAURY <jeffmaury.github@jeffmaury.com>
2013-04-23 22:59:58 +0200

    Suppress the Add Xtext nature dialog displayed each time a Golo file is opened

--- org.gololang.gldt.core.ui/src/main/java/org/gololang/gldt/core/ui/GoloUiModule.java
+++ org.gololang.gldt.core.ui/src/main/java/org/gololang/gldt/core/ui/GoloUiModule.java
@@ -4,6 +4,7 @@
 package org.gololang.gldt.core.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.IXtextEditorCallback;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -12,4 +13,12 @@ public class GoloUiModule extends org.gololang.gldt.core.ui.AbstractGoloUiModule
 	public GoloUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
 	}
+	
+	/**
+	 * Suppress the Add Xtext nature dialog
+	 */
+	public Class<? extends IXtextEditorCallback> bindIXtextEditorCallback() {
+		return IXtextEditorCallback.NullImpl.class;
+	}
+
 }
